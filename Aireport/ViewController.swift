3063380c206bb830904b8c1ed7667a41c71a4fe8
纪洪波 <hongbo.ji@wenba100.com>
2018-03-26 17:49:18 +0800

    add color

--- Aireport/ViewController.swift
+++ Aireport/ViewController.swift
@@ -23,6 +23,26 @@ class ViewController: UIViewController {
     func gateDate() {
         AirAPI.getAirReport { (airModel) in
             self.aqiLable.text = "\(airModel.aqi)";
+            switch airModel.aqi {
+            case 0...50:
+                self.aqiLable.textColor = #colorLiteral(red: 0.1697491109, green: 0.5999971032, blue: 0.3989009857, alpha: 1)
+                break
+            case 51...100:
+                self.aqiLable.textColor = #colorLiteral(red: 0.9837319255, green: 0.8724730611, blue: 0.1980268359, alpha: 1)
+                break
+            case 101...150:
+                self.aqiLable.textColor = #colorLiteral(red: 0.9706934094, green: 0.5985591412, blue: 0.2020688355, alpha: 1)
+                break
+            case 151...200:
+                self.aqiLable.textColor = #colorLiteral(red: 0.7984669805, green: 0.01951491833, blue: 0.1984502971, alpha: 1)
+                break
+            case 201...300:
+                self.aqiLable.textColor = #colorLiteral(red: 0.3991004229, green: 0, blue: 0.6014233828, alpha: 1)
+                break
+            default:
+                self.aqiLable.textColor = #colorLiteral(red: 0.492726028, green: 0.006434844341, blue: 0.1375600398, alpha: 1)
+                break
+            }
         }
     }
 
