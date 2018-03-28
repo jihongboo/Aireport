868c0ebb3e1072e4e212becfd783687ffb61ed07
纪洪波 <jihongboo@qq.com>
2018-03-29 00:34:59 +0800

    change background fetch duration to 30min

--- Aireport/AppDelegate.swift
+++ Aireport/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         registerPush()
-        UIApplication.shared.setMinimumBackgroundFetchInterval(60 * 60)
+        UIApplication.shared.setMinimumBackgroundFetchInterval(60 * 30)
         return true
     }
 
